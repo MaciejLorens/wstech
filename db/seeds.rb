fb8166a4f7bcea56d808f7d2f3e682591408e406
Maciej Lorens <maciej.lorens@gmail.com>
2019-06-11 16:24:02 +0200

    limit editing orders to admin for ordered and above statuses

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,18 @@
-maciej = User.create(first_name: 'Maciej', last_name: 'Lorens', email: 'mckl@poczta.fm', password: 'wstechmaciej', admin: true)
-jacek = User.create(first_name: 'Jacek', last_name: 'Mazur', email: 'jmazur@pro.onet.pl ', password: 'wstechjacek', admin: true)
-tomek = User.create(first_name: 'Tomasz', last_name: 'Somer', email: 'tomasz@somer.pl', password: 'wstechtomasz', admin: false)
+maciej = User.create(first_name: 'Maciej', last_name: 'Lorens', email: 'mckl@poczta.fm', password: '1234567890', admin: true)
+jacek = User.create(first_name: 'Jacek', last_name: 'Mazur', email: 'jmazur@pro.onet.pl ', password: '1234567890', admin: true)
+tomek = User.create(first_name: 'Tomasz', last_name: 'Somer', email: 'tomasz@somer.pl', password: '1234567890', admin: false)
 
-client = User.create(first_name: 'Franek', last_name: 'Golas', email: 'franek@golas.fm', password: 'password', admin: false)
+client = User.create(first_name: 'Franek', last_name: 'Golas', email: 'franek@golas.fm', password: '1234567890', admin: false)
 
-Order.create(type: 'MetalOrder', user_id: client.id, description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat', quantity: 17, price: 234.77, delivery_request_date: Time.now + 3.days, confirmation_date: Time.now + 4.days)
-Order.create(type: 'MetalOrder', user_id: client.id, description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua', quantity: 9, price: 123.74, delivery_request_date: Time.now + 1.days, confirmation_date: Time.now + 1.days)
-Order.create(type: 'MetalOrder', user_id: client.id, description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', quantity: 17, price: 234.77, delivery_request_date: Time.now + 3.days, confirmation_date: Time.now + 4.days)
-Order.create(type: 'MetalOrder', user_id: client.id, description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat', quantity: 9, price: 123.74, delivery_request_date: Time.now + 1.days, confirmation_date: Time.now + 1.days)
-Order.create(type: 'FurnitureOrder', user_id: client.id, description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', quantity: 4, price: 1393.20, delivery_request_date: Time.now + 7.days, confirmation_date: Time.now + 9.days)
-Order.create(type: 'FurnitureOrder', user_id: client.id, description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat', quantity: 11, price: 13546.32, delivery_request_date: Time.now + 12.days, confirmation_date: Time.now + 12.days)
-Order.create(type: 'FurnitureOrder', user_id: client.id, description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam', quantity: 1, price: 199.11, delivery_request_date: Time.now + 5.days, confirmation_date: Time.now + 7.days)
-
+100.times do
+  Order.create(
+    type: %w(MetalOrder FurnitureOrder).sample,
+    user_id: client.id,
+    description: 'Lorem ipsum dolor sit amet ' + SecureRandom.hex(10),
+    quantity: rand(100),
+    price: rand(10000).to_f / 100,
+    delivery_request_date: Time.now + rand(5).days,
+    confirmation_date: Time.now + rand(20).days,
+    status: %w(inquiry proposition not_confirmed ordered delivered deleted).sample
+  )
+end
