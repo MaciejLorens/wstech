9549947a805442f0a80aa8ab4209522adb998763
Maciej Lorens <maciej.lorens@gmail.com>
2019-06-12 16:56:43 +0200

    add dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,12 @@
 class HomeController < ApplicationController
 
   def index
+    from = params[:from] || 1.month.ago.beginning_of_day
+    to = params[:to] || Time.now.end_of_day
+
+    @metal_orders = Order.metal.where('delivery_date >= ? AND delivery_date <= ?', from, to)
+    @furniture_orders = Order.furniture.where('delivery_date >= ? AND delivery_date <= ?', from, to)
+    @orders = Order.where('delivery_date >= ? AND delivery_date <= ?', from, to)
   end
 
   def search
